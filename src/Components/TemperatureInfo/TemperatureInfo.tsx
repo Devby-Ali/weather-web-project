a921cd49459ac073edc5b0e01bd9243c1d043a5e
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-04-26 18:17:59 +0330

    Responsive TemperatureInfo Component

--- src/Components/TemperatureInfo/TemperatureInfo.tsx
+++ src/Components/TemperatureInfo/TemperatureInfo.tsx
@@ -3,20 +3,22 @@ import { IoLocation } from "react-icons/io5";
 
 const TemperatureInfo = (): React.JSX.Element => {
   return (
-    <section className="flex justify-between w-full lg:w-[607px] h-[234px] px-6 py-5 shadow-custom text-darkText dark:text-lightText mb-7 lg:mb-0">
-      <div className="flex flex-col max-w-48 mr-3 mt-1.5">
+    <section className="flex justify-between w-full lg:w-[607px] h-[234px] px-6 py-5 shadow-custom text-darkText dark:text-lightText mb-7 lg:mb-0 font-Roboto-regular">
+      <div className="flex flex-col items-end max-w-48 mr-3 mt-1.5 font-Inter-regular">
         <img src="/images/svgs/image7.svg" alt="img" />
-        <span className="text-[33px] tracking-wide -mr-1.5 mt-1">Cloudy</span>
-        <span className="tracking-wide">Feels Like 26</span>
+        <div className="-ml-5 mt-1 flex flex-col items-end">
+          <span className="text-[32px]">Cloudy</span>
+          <span>Feels Like 26</span>
+        </div>
       </div>
       <div>
-        <span className="flex items-center justify-between tracking-wide text-[#3D4852] bg-[#CDD9E0] rounded-4xl w-[173px] h-10 pr-[25px]">
+        <span className="flex items-center justify-between text-base font-Inter-regular text-[#3D4852] bg-[#CDD9E0] rounded-4xl w-[173px] h-10 pl-2.5 pr-[25px]">
           San Francisco
-          <IoLocation className="text-2xl ml-2" />
+          <IoLocation className="text-2xl" />
         </span>
         <div className="ltr-text mt-2.5">
-          <span className="text-[32px] font-bold tracking-tight">Monday</span>
-          <div className="flex text-[14px] gap-x-4 -mt-1">
+          <span className="text-[32px]">Monday</span>
+          <div className="flex text-[14px]/4 font-Roboto-light gap-x-5">
             <span>24 Dec, 2023</span>
             <span>11:45 AM</span>
           </div>
@@ -24,7 +26,7 @@ const TemperatureInfo = (): React.JSX.Element => {
         <div className="ltr-text mt-2">
           <div className="flex items-center gap-x-2">
             <span className="text-[40px]">26</span>
-            <span className="inline-block w-4 h-4 rounded-full border-[3px] border-darkText dark:border-lightText mb-5"></span>
+            <span className="inline-block w-4 h-4 rounded-full border-[3px] border-darkText dark:border-lightText mb-4.5 mr-1"></span>
             <span className="text-[40px]">C</span>
           </div>
           <div className="flex text-[14px] gap-x-1 -mt-2">
