70a9b050e9ec876ab9136228aad10e6684f32046
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-02 19:06:46 +0330

    Setting up i18n in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
 import { WeatherProvider } from "./Contexts/WeatherContext";
+import './i18n'
 
 
 
 const App = (): React.JSX.Element => {
+
   useEffect(() => {
     if (localStorage.theme === "dark") {
       document.documentElement.classList.add("dark");
@@ -13,11 +15,15 @@ const App = (): React.JSX.Element => {
       document.documentElement.classList.remove("dark");
     }
   }, []);
+
   const router = useRoutes(routes);
+
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <WeatherProvider>
       {router}
     </WeatherProvider>
+    </Suspense>
   );
 }
 
