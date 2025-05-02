518e6a5b8d9fffcd82bbae42d0c94492819e530e
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-02 19:04:20 +0330

    Configuration i18n

---
+++ src/i18n.ts
@@ -0,0 +1,31 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage']
+    }
+  });
+
+// تغییر خودکار جهت صفحه
+i18n.on('languageChanged', (lng) => {
+  document.documentElement.dir = lng === 'fa' ? 'rtl' : 'ltr';
+  document.documentElement.lang = lng;
+});
+
+export default i18n;
