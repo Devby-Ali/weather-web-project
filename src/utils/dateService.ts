a169de239986fb2fc2ad9e929e95757fbbaf01ca
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-04 18:02:50 +0330

    Coding getLocalizedMonth Func dateService.ts

--- src/utils/dateService.ts
+++ src/utils/dateService.ts
@@ -20,7 +20,7 @@ export const getFormattedDateTime = () => {
   moment.locale(isPersian ? 'fa' : 'en');
   return {
     dayMonth: toPersianNumbers(moment().jDate()),
-    dayName: moment().format('dddd'), // استفاده از فرمت استاندارد
+    dayName: moment().format('dddd'), 
     fullDate: isPersian
       ? `${toPersianNumbers(moment().jYear())} ${moment().format('jMMMM')}`
       : moment().format('DD MMM, YYYY'),
@@ -42,4 +42,17 @@ export const formatTemp = (temp: number) => {
   return i18n.language === 'fa' 
     ? toPersianNumbers(Math.round(temp))
     : Math.round(temp);
+};
+
+export const getLocalizedMonth = (monthKey: string, isPersian: boolean): string => {
+  if (!isPersian) return monthKey;
+
+
+  const monthIndexMap: { [key: string]: number } = {
+    Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5,
+    Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11
+  };
+
+  const index = monthIndexMap[monthKey];
+  return moment().jMonth(index).format('jMMMM');
 };
