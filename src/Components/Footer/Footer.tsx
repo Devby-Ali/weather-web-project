346ee319c351b342f61e17bb3b0255d7d8510086
Ali Bakhshi <alibakhshi962002@gmail.com>
2025-05-05 15:35:59 +0330

    Set Footer component translation

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { LuCalendarDays } from "react-icons/lu";
+import { getFormattedDateTime } from "../../utils/dateService";
+import { useTranslation } from "react-i18next";
 
 const Footer = (): React.JSX.Element => {
+  const { t, i18n } = useTranslation();
+  const { dayName, fullDate, time, dayMonth } = getFormattedDateTime();
+  const isPersian = i18n.language === "fa";
   return (
     <footer className="flex flex-col lg:flex-row mt-25 px-6 lg:items-center justify-center lg:justify-between w-full h-26 bg-gradient-to-r dark:from-[#292F45] dark:via-[#3F4861] dark:to-[#151D32] from-[#F3FAFE] via-[#CCDDDD9E] to-[#F3FAFE] text-darkText dark:text-lightText">
       <div className="flex items-center gap-x-3">
         <img src="/images/svgs/icon-copy-1.png" alt="" />
         <span className="mb-1 text-xs lg:text-sm">
-          All rights of this site are reserved for Nadin Sadr Aria Engineering
-          Company.
+        {t("footer.desc")}
         </span>
       </div>
       <div className="flex flex-col sm:flex-row items-start gap-x-6 lg:gap-x-11 sm:self-end lg:self-auto">
         <div className="flex-center gap-x-1.5 lg:text-[23px]">
           <MdOutlineMailOutline />
           <span className="text-xs lg:text-sm mb-1">
-            contact us : info@nadin.ir
+            {t("footer.contact_us")} : info@nadin.ir
           </span>
         </div>
         <div className="flex-center gap-x-1.5 lg:text-[22px]">
           <LuCalendarDays />
-          <span className="text-xs lg:text-sm mb-1">
-            <span className="tracking-wide">12:25</span> . Monday 23 December
-            2023
-          </span>
+          <div className="flex-center text-xs lg:text-sm mb-1">
+            <span className="tracking-wide">{time}</span> . 
+            <span className="mx-1">{dayName}</span>
+            <div className="flex-center ltr gap-x-1">
+              <span>{fullDate}</span>
+              {i18n.language === "fa" && <span>{dayMonth}</span>}
+            </div>
+          </div>
         </div>
       </div>
     </footer>
